de3ea68f74be114275f9d6511adf8a43c721ee20
Lyubomir Svilenov <svilenov.lubomir@gmail.com>
2020-11-01 00:22:03 +0200

    Implementing RabbitMQ Bus

--- MicroRabbit/Domain/MicroRabbit.Domain.Core/Bus/IEventBus.cs
+++ MicroRabbit/Domain/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -7,9 +7,9 @@ namespace MicroRabbit.Domain.Core.Bus
 {
     public interface IEventBus
     {
-        Task SendCommand<T>(T command) where T : Command;
-
         void Publish<T>(T @event) where T : Event;
+
+        Task SendCommand<T>(T command) where T : Command;
 
         void Subscribe<T, TH>()
             where T : Event
