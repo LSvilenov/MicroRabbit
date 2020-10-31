635f9e1015b51110f3bd3b36eecfdd23298c38e6
Lyubomir Svilenov <svilenov.lubomir@gmail.com>
2020-10-31 23:21:37 +0200

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroRabbit/Domain/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Events;
+
+using System;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        protected Command()
+        {
+            this.TimeStamp = DateTime.UtcNow;
+        }
+
+        public DateTime TimeStamp { get; protected set; }
+    }
+}
